639991fc00de0be29f633631b8dbd30713fabaeb
Bharat Gupta <43195065+bharatgupta77@users.noreply.github.com>
2025-05-02 17:16:13 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_7rbEUQIwW', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4fs1qbb7a4s1th88dta6cn8i8h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
